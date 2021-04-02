abf3446cd6eb786496f8f13aae463051e9b6378d
Lewis Rotchell <rotchell92@gmail.com>
2021-04-02 19:01:04 +0100

    commit

---
+++ src/Modal.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const modalRoot = document.getElementById("modal");
+
+const Modal = ({ children }) => {
+  //a ref is state that you want to survive past render cycles(don't want to create new divs every time the omponent re renders)
+  const elRef = useRef(null);
+  if (!elRef.current) {
+    elRef.current = document.createElement("div");
+  }
+
+  useEffect(() => {
+    modalRoot.appendChild(elRef.current);
+    return () => modalRoot.removeChild(elRef.current);
+  });
+
+  return createPortal(<div>{children}</div>, elRef.current);
+};
+
+export default Modal;
